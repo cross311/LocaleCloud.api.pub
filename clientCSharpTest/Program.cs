39b0e38d84c7c6bf872f0543573c75a70d278626
Connor Ross <cross@mdsol.com>
2013-05-19 19:15:10 -0400

    started test of csharp client

---
+++ clientCSharpTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using medidata.localeCloud.api.pub.client.csharp;
+
+namespace clientCSharpTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new Client();
+
+            TryLogin(client);
+
+            client.CreateApp("TestApp", "en-us", "Release1", true);
+
+            var apps = client.GetApps();
+
+            Console.WriteLine("Successfuly created test311 user and TestApp application.");
+        }
+
+        static void TryLogin(Client client)
+        {
+            try
+            {
+                client.Login("test311", "P@$$w0rd!");
+            }
+            catch (Exception)
+            {
+                var create = client.CreateUser("Test", "Test", "test@test.com", "test311", "P@$$w0rd!");
+
+                var login = client.Login("test311", "P@$$w0rd!");
+            }
+        }
+    }
+}
--- medidata.localeCloud.api.pub.client.csharp/Client.cs
+++ medidata.localeCloud.api.pub.client.csharp/Client.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using medidata.localeCloud.api.pub.dtos;
 using medidata.localeCloud.api.pub.dtos.models;
+using ServiceStack.Common.ServiceClient.Web;
 using ServiceStack.ServiceClient.Web;
 
 namespace medidata.localeCloud.api.pub.client.csharp
@@ -14,16 +15,66 @@ namespace medidata.localeCloud.api.pub.client.csharp
         ServiceClientBase m_client;
 
         public Client()
-            : this("http://localhost/api") { }
+            : this("http://localhost:51260/localecloud/api/") { }
 
         public Client(string baseUri)
         {
             m_client = new JsonServiceClient(baseUri);
         }
 
+        #region User
+        public RegistrationResponse CreateUser(string firstName, string lastName, string email, string username, string password)
+        {
+            return m_client.Post(new Registration
+            {
+                AutoLogin = false,
+                DisplayName = string.Format("{0}, {1}", lastName, firstName),
+                Email = email,
+                FirstName = firstName,
+                LastName = lastName,
+                Password = password,
+                UserName = username
+            });
+        }
+
+        public AuthResponse Login(string username, string password)
+        {
+            return m_client.Post(new Auth
+            {
+                UserName = username,
+                Password = password,
+                RememberMe = true,
+                provider = "credentials"
+            });
+        }
+        #endregion
+        #region App
         public List<App> GetApps()
         {
             return m_client.Get(new Apps());
         }
+
+        public void CreateApp(string name, string baseLocale, string startReleaseName, bool allowAutoCreate)
+        {
+            m_client.Put(new SaveApp
+            {
+                AppName = name,
+                AllowAutoCreate = allowAutoCreate,
+                BaseLocale = baseLocale,
+                StartReleaseName = startReleaseName
+            });
+        }
+        #endregion
+        #region Permission
+        public void GivePermission(string app, string username, PermissionType permission)
+        {
+            m_client.Put(new ModifyPermissions
+            {
+                AppName = app,
+                PermissionType = permission,
+                Username = username
+            });
+        }
+        #endregion
     }
 }
