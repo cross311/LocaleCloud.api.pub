220d272f333768c9a51d2427057e7d0273d40b32
Connor Ross <cross@mdsol.com>
2013-05-19 16:58:35 -0400

    Added start of csharp client

---
+++ medidata.localeCloud.api.pub.client.csharp/Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using medidata.localeCloud.api.pub.dtos;
+using medidata.localeCloud.api.pub.dtos.models;
+using ServiceStack.ServiceClient.Web;
+
+namespace medidata.localeCloud.api.pub.client.csharp
+{
+    public class Client
+    {
+        ServiceClientBase m_client;
+
+        public Client()
+            : this("http://localhost/api") { }
+
+        public Client(string baseUri)
+        {
+            m_client = new JsonServiceClient(baseUri);
+        }
+
+        public List<App> GetApps()
+        {
+            return m_client.Get(new Apps());
+        }
+    }
+}
